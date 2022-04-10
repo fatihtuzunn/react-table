ffcdc5cb886bf7379e842d865c97552ed8470e03
Fatih Tuzun <fatihtuzun11@gmail.com>
2022-04-10 23:39:13 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import  data  from "./data.json";
+import { nanoid } from "nanoid";
 
 function App() {
+const [contacts, setContacts] = useState(data);
+const [addFormData, setAddFormData] = useState({
+  fullName: '',
+  address: '',
+  phone: '',
+  email: ''
+
+});
+
+  const handleAddFormChange = (event) =>{
+    event.preventDefault();
+
+    const fieldName = event.target.getAttribute('name');
+    const fieldValue = event.target.value;
+
+    const newFormData = {...addFormData};
+    newFormData[fieldName] = fieldValue;
+
+    setAddFormData(newFormData);
+  }
+
+  const handleAddFormSubmit = (event)=>{
+    event.preventDefault();
+
+    const newContact={
+      id: nanoid(), 
+      fullName: addFormData.fullName,
+      address: addFormData.address,
+      phone: addFormData.phone,
+      email: addFormData.email
+    }
+
+    const newContacts = [...contacts, newContact]
+    setContacts(newContacts);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Adress</th>
+            <th>Phone</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+
+        {contacts.map((contact)=>(
+          <tbody>
+          <tr>
+            <td>{contact.fullName}</td>
+            <td>{contact.adress}</td>
+            <td>{contact.phone}</td>
+            <td>{contact.email}</td>
+          </tr>
+        </tbody>
+        ))}
+        
+      </table>
+
+      <form onSubmit={handleAddFormSubmit} action="">
+          <input 
+          type="text" 
+          name="fullName" 
+          required="required"
+          placeholder='enter a name'
+          onChange={handleAddFormChange}  />
+          <input 
+          type="text" 
+          name="address" 
+          required="required"
+          placeholder='enter a address' 
+          onChange={handleAddFormChange} />
+          <input 
+          type="text" 
+          name="phone" 
+          required="required"
+          placeholder='enter a phone' 
+          onChange={handleAddFormChange} />
+          <input 
+          type="text" 
+          name="email" 
+          required="required"
+          placeholder='enter a email' 
+          onChange={handleAddFormChange} />
+          <button type='submit'>ADD</button>
+
+      </form>
     </div>
   );
 }
